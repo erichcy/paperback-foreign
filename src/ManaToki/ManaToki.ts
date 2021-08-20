01f008a4e928a89338683237588c62701e5ed1fa
BKLFIRST <89240929+BKLFIRST@users.noreply.github.com>
2021-08-20 20:04:51 +0900

    URL Change and version up (#51)

--- src/ManaToki/ManaToki.ts
+++ src/ManaToki/ManaToki.ts
@@ -12,13 +12,13 @@ import {
 } from "paperback-extensions-common"
 import {parseChapterDetails, parseChapters, parseHomeUpdates, parseHomeList, parseMangaDetails, parseSearch, parseUpdatedMangas} from "./ManaTokiParser"
 
-const MANATOKI_DOMAIN = 'https://manatoki95.net'
+const MANATOKI_DOMAIN = 'https://manatoki96.net'
 const MANATOKI_COMIC = MANATOKI_DOMAIN + '/comic/'
 const method = 'GET'
 
 
 export const ManaTokiInfo: SourceInfo = {
-    version: '1.0.4',
+    version: '1.0.5',
     name: '마나토끼',
     icon: 'icon.png',
     author: 'nar1n',
